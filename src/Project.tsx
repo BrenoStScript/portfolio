81cf6cff0f20090b9d64b005c3024b182b4bba15
Breno Santos <brenostscript@gmail.com>
2023-05-24 18:26:17 -0300

    title change and project added

--- src/Project.tsx
+++ src/Project.tsx
@@ -1,4 +1,5 @@
 import logoRoxo from './assets/Logo.png'
+import vaccineCard from './assets/vaccine-card.jpg'
 import './Projects.css'
 
 
@@ -7,10 +8,10 @@ function Project(){
         <div className="projects">
             <div className='project'>
                 <div className='project-content'>
-                    <h1>NextTale</h1>
-                    <p>Project made with React + Vite.<br/> Consuming the IMDB API the site gives recomendations based on the last show you whatched  </p>
-                    <a>
-                        <img src={logoRoxo}/>
+                    <h1>Vaccine-Card</h1>
+                    <p>Project made with JavaScript.<br/> Based on the brazilian child vaccination callendar, input a birthdate and get to now the status of the child on the callendar.</p>
+                    <a href='https://vaccine-card.vercel.app/' target='blank'>
+                        <img src={vaccineCard}/>
                     </a>
                 </div>
             </div>
